929025e0b138b3a29c04aaf6fcf5f18e89d18982
DAVID-DARA <davidfamoyegun1@gmail.com>
2025-03-05 18:14:12 +0100

    React Router added an tested

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import styles from "./HomePage.module.css"
+
+const HomePage = () => {
+    return (
+        <>
+            <h1 className={styles.h1Styles}>HomePages</h1>
+        </>
+    );
+}
+
+export default HomePage;
